6cee7a9e88b4fd66c03975efea2ce48db854e7dd
Mael <legrand.mael2002@gmail.com>
2022-06-24 13:24:30 +0200

    add docstring

--- supervision.py
+++ supervision.py
@@ -8,19 +8,28 @@ import psutil
 # data = "testdata,data=cpu number=8"
 now = datetime.datetime.now()
 
+
 def send_cpu():
+    """
+    Send CPU data
+    :return: CPU data as list
+    """
     # CPU
     print("   > CPU ")
-    cpu_time = 'cpu,data=cpu cpu_time=' + str(psutil.cpu_times()[0]) + ' time=' + str(datetime.datetime.timestamp(now))
+    cpu_time = 'cpu,data=cpu cpu_time=' + str(psutil.cpu_times()[0])
     percent = 'cpu,data=cpu percent=' + str(psutil.cpu_percent())
     count = 'cpu,data=cpu count=' + str(psutil.cpu_count())
     time_percent = 'cpu,data=cpu time_percent=' + str(psutil.cpu_times_percent(percpu=False).user)
     frequency = 'cpu,data=cpu frequency=' + str(psutil.cpu_freq().current)
     load_avg = 'cpu,data=cpu load_avg=' + str(psutil.getloadavg()[0])
-    return [cpu_time]
+    return [cpu_time, percent, count, time_percent, frequency, load_avg]
 
 
 def send_memory():
+    """
+    Send memory data
+    :return: memory data as list
+    """
     # Memory
     print("   > Memory ")
     memory = "memory,data=memory virtual=" + str(psutil.virtual_memory().total)
@@ -30,6 +39,10 @@ def send_memory():
 
 
 def send_disk():
+    """
+    Send disk data
+    :return: Disk data as list
+    """
     # Disk
     print("   > Disk ")
     usage = "disk,data=disk usage=" + str(psutil.disk_usage('/').total)
@@ -37,6 +50,10 @@ def send_disk():
 
 
 def send_network():
+    """
+    Send network data
+    :return: Network data as list
+    """
     # Network
     print("   > Network ")
     counters = "network,data=network io counters=" + str(psutil.net_io_counters(pernic=True))
@@ -46,6 +63,10 @@ def send_network():
 
 
 def send_sensors():
+    """
+    Send sensors data
+    :return: Sensors data as list
+    """
     # Sensors
     print("   > Sensors ")
     battery = "sensors,data=sensors battery=" + str(psutil.sensors_battery().percent)
@@ -53,6 +74,10 @@ def send_sensors():
 
 
 def send_other():
+    """
+    Send other data
+    :return: some other data as list
+    """
     # Other
     print("   > Other")
     boot = "other,data=boot boot_time=" + str(psutil.boot_time())
@@ -60,6 +85,10 @@ def send_other():
 
 
 def send_all():
+    """
+    Send all data as list of function
+    :return: functions as list
+    """
     # Send all data
     print(" >>> Send all data: ")
     return [send_cpu(), send_memory(), send_disk(), send_sensors(), send_other()]
